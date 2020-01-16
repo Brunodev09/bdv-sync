a8ae77851f1c911280749a83a5bbeeccc63543a8
Brunodev09 <bruno.mayol.giannotti@gmail.com>
2020-01-16 17:10:57 -0300

    Working with channels.

--- server/src/controllers/CreateChannel.ts
+++ server/src/controllers/CreateChannel.ts
@@ -5,9 +5,12 @@ import User from '../models/User';
 import _ from "../tools/Terminal";
 import { IUser } from "../interfaces/User";
 import mongoose from "mongoose";
+import { Channel } from '../interfaces/Channel';
+
+import Struct from "../struct.json";
 
 
-export default class UserController {
+export default class CreateChannel {
     public path = '/user';
     public router = express.Router();
 
@@ -22,6 +25,18 @@ export default class UserController {
     }
 
     create = async (request: express.Request, response: express.Response) => {
-        
+        try {
+            let { channel: { dumpData, participants, size  } } = request.body;
+            const id = CreateChannel.getNextChannelId();
+            const channel = new Channel(id, participants, size, dumpData, request.user._id);
+            
+        } catch (e) {
+            _.say(`User ${request.user.email} has requested the create channel route with invalid parameters!`);
+            return response.status(400).json({error: "Invalid set of paramaters for Channel creation!"});
+        }
+    }
+
+    static getNextChannelId() {
+        return Number(Struct.storages[Struct.storages.length - 1].channel) + 1;
     }
 }
--- server/src/interfaces/Channel.ts
+++ server/src/interfaces/Channel.ts
@@ -1,10 +1,23 @@
 import mongoose from "mongoose";
+import { IUser } from "./User";
 
-export default interface IChannel {
+export interface IChannel {
     id: number;
     participants: mongoose.Schema.Types.ObjectId[],
     size: number;
     dumpData: string,
     lastUpdated: Date;
     lastUpdater: mongoose.Schema.Types.ObjectId
+}
+
+export class Channel implements IChannel {
+    public id: number;
+    public participants: mongoose.Schema.Types.ObjectId[];
+    public size: number;
+    public dumpData: string;
+    public lastUpdater: IUser;
+    lastUpdated: Date;
+    constructor(id, participants, size, dumpData, lastUpdater) {
+        this.lastUpdated = new Date();
+    }
 }
